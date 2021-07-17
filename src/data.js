5117a02b3e3894d1051a1b22da2626a8b51e51c9
123Nira <77059367+123Nira@users.noreply.github.com>
2021-07-17 16:36:55 +0530

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,26 @@
+const data = {
+  products: [
+    {
+      id: "1",
+      name: "HPLaptop",
+      price: 52000,
+      image:
+        "https://hk-media.apjonlinecdn.com/catalog/product/cache/b3b166914d87ce343d4dc5ec5117b502/c/0/c06539619_1_1.png"
+    },
+    {
+      id: "2",
+      name: "Tesla Car",
+      price: 6000000,
+      image:
+        "https://www.teslarati.com/wp-content/uploads/2019/07/tesla-model-s-raven-2.jpg"
+    },
+    {
+      id: "3",
+      name: "W Shoes",
+      price: 1000,
+      image:
+        "https://cdn.shopify.com/s/files/1/1414/2498/products/CS_ClassicWhite_06_800x.jpg?v=1614938767"
+    }
+  ]
+};
+export default data;
